7711cf770b187c9d5205845ef1a8e06d748fbf6d
Malaika Riasat <malaika.202404824@gmail.com>
2026-06-24 01:34:03 +0500

    js

--- javascript.js/ToDoapp/todo.js
+++ javascript.js/ToDoapp/todo.js
@@ -1,2 +1,15 @@
 console.log("java script is live Know");
-document.getElementById("addbtn");
+
+let inputbox= document.getElementById("attendance");
+let btnbox= document.getElementById("addStudent");
+let container= document.getElementById("studentContainer");
+let deletbtn = document.createElement("button");
+deletbtn.textContent="Delete";
+
+btnbox.addEventListener("click",function(){
+    let student = document.createElement("div");
+    student.textContent = inputbox.value;
+    console.log(inputbox.value);
+    inputbox.value="";
+
+});

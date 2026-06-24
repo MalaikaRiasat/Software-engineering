a64bd36637c3f8b3abc516f3062c42d9ba1539a8
Malaika Riasat <malaika.202404824@gmail.com>
2026-06-24 11:38:17 +0500

    commit

--- javascript.js/ToDoapp/todo.js
+++ javascript.js/ToDoapp/todo.js
@@ -1,14 +1,21 @@
-console.log("java script is live Know");
-
 let inputbox= document.getElementById("attendance");
 let btnbox= document.getElementById("addStudent");
 let container= document.getElementById("studentContainer");
-let deletbtn = document.createElement("button");
-deletbtn.textContent="Delete";
 
 btnbox.addEventListener("click",function(){
+
     let student = document.createElement("div");
     student.textContent = inputbox.value;
+
+    let deletbtn = document.createElement("button");
+    deletbtn.textContent="Delete";
+
+    deletbtn.addEventListener("click",function(){
+        student.remove();
+    });
+
+    student.appendChild(deletbtn);
+    container.appendChild(student)
     console.log(inputbox.value);
     inputbox.value="";
 

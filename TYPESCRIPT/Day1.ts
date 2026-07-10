b1307494924502ae67700b77474d983ac4b9a6db
Malaika Riasat <malaika.202404824@gcuf.edu.pk>
2026-07-10 22:49:12 +0500

    commit

---
+++ TYPESCRIPT/Day1.ts
@@ -0,0 +1,4 @@
+interface User {
+  name: string;  // Must be text
+  id: number;    // Must be a number
+}
---
+++ TYPESCRIPT/index.ts
@@ -0,0 +1,10 @@
+let name:String="Malika";
+console.log(name);
+
+//string type only
+let college:string="gcuf";
+console.log(college);
+
+//number
+let age:number=21;
+console.log(age);

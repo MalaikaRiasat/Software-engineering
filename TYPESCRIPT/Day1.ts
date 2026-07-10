3670ab30366d537f948236fa202d566238365db3
Malaika Riasat <malaika.202404824@gcuf.edu.pk>
2026-07-10 22:50:54 +0500

    commit

--- TYPESCRIPT/Day1.ts
+++ TYPESCRIPT/Day1.ts
@@ -2,3 +2,12 @@ interface User {
   name: string;  // Must be text
   id: number;    // Must be a number
 }
+
+type ID = number | string;
+
+function printID(id: ID) {
+  console.log("Your ID is: " + id);
+}
+
+printID(101);   // Totally fine!
+printID("A-99"); // Also totally fine!

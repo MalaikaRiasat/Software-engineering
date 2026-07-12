6272910ab51711344428385f47230a93093a57dd
Malaika Riasat <malaika.202404824@gcuf.edu.pk>
2026-07-13 00:10:54 +0500

    commit

--- typescript/src/index.ts
+++ typescript/src/index.ts
@@ -1,4 +1,26 @@
-name="Malaika";
+let name="Malaika";  // name is string
 console.log(typeof(name));
 
 console.log(name);
+// name = 19;   // Error becuse name value is string and it can not change there datatype
+console.log(name);
+
+
+
+//Type inference
+let age=21;  // age is number
+// age="twenty" error because age value in number not a string
+
+console.log(age);
+
+
+//interface
+//commanly used with objects
+
+
+interface student{
+    rollNO: number;
+    class: string;
+    contactNo: number;
+}
+// its mean that every student must have there rollNO in numbers, class in string and the last one contact number in numbers

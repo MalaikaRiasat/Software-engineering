96c3a350becc38d2d32b949ad12a1afe698fb8ee
Malaika Riasat <malaika.202404824@gcuf.edu.pk>
2026-07-14 00:03:49 +0500

    coomit

---
+++ test typescript.ts/index.js
@@ -0,0 +1,6 @@
+"use strict";
+const user_1 = {
+    name: "Malaika",
+    email: "@malaikaRiasat",
+    phoneNO: +9237864685,
+};
---
+++ test typescript.ts/index.ts
@@ -0,0 +1,13 @@
+interface User{
+    name: string;
+    email:string;
+    phoneNO:number;
+}
+const user_1:User={
+    name:"Malaika",
+    email:"@malaikaRiasat",
+    phoneNO: +9237864685,
+}
+export{
+    user_1,
+}

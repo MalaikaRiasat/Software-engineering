baa8400fde7b2da8c48d482223f4bf52fe762b9b
Malaika Riasat <malaika.202404824@gcuf.edu.pk>
2026-07-14 11:24:23 +0500

    Add new Software Engineering project

---
+++ javascript.js/Arry.js
@@ -0,0 +1,25 @@
+// let school=["student","teachers"]
+// console.log (school);
+
+
+// pop
+// let school=["student","teachers"];
+// school.pop();
+// console.log(school);  //last remove
+
+//shift()
+
+// let school=["student","teachers"];
+// school.shift();
+// console.log(school);   //1st sa remove
+
+//unshift
+
+// let fruits = ["apple", "banana"];
+// fruits.unshift("orange", "mango");  //add at 1st in arry
+// // fruits["orange", "mango", "apple", "banana"]
+
+// //push
+// let colors = ["red", "blue"];  
+// colors.push("green");  //add at the last in arry
+// // colors is now ["red", "blue", "green"]
---
+++ javascript.js/fun.js
@@ -0,0 +1,14 @@
+function school(){
+    console.log("hello");
+}
+school();
+
+//function into function
+function out(){
+    function inn(){
+        console.log("inner");
+    }
+    inn();
+}
+
+out();

8e89f9fabea00753e8f8b6e6ea1b9494f088d747
Malaika Riasat <malaika.202404824@gcuf.edu.pk>
2026-07-15 23:55:12 +0500

    commit

---
+++ Typescript/index.ts
@@ -0,0 +1,15 @@
+interface User {
+    name: string;
+    class: number;
+    phoneNO: string;
+} 
+
+const user:User ={
+    name:"Malaika",
+    class: 13,
+    phoneNO:"028918"
+} 
+
+export{
+    user
+}

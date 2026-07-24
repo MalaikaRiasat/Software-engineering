5df6e105f6f7fa70248db59d53a0ca043720ddfd
Malaika Riasat <malaika.202404824@gcuf.edu.pk>
2026-07-24 10:34:38 +0500

    c

--- My-first-react-vite-project/src/App.tsx
+++ My-first-react-vite-project/src/App.tsx
@@ -7,15 +7,15 @@ export type TabKey = "notifications" | "network";
 
 function App() {
   const [activeTab, setActiveTab] = useState<TabKey>("notifications");
-
   return (
     <div className="min-h-screen bg-gray-100">
-      <Navbar activeTab={activeTab} setActiveTab={setActiveTab} />
 
+      <Navbar activeTab={activeTab} setActiveTab={setActiveTab} />
       <main className="max-w-3xl mx-auto mt-6 px-4">
         {activeTab === "notifications" && <NotificationsPage />}
         {activeTab === "network" && <NetworkPage />}
       </main>
+
     </div>
   );
 }

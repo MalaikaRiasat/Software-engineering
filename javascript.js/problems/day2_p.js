69152b76ac81e09642d1a0d430e2db2bb2abfbb6
Malaika Riasat <malaika.202404824@gcuf.edu.pk>
2026-07-28 17:08:09 +0500

    js

---
+++ javascript.js/problems/day2_p.js
@@ -0,0 +1,29 @@
+// Problem 1 — sirf && (AND)
+// Ek library system: user ko book issue tabhi ho jab:
+
+// hasLibraryCard (boolean) true ho, aur
+// booksAlreadyIssued (number) 3 se kam ho
+
+// Agar dono true/valid hain → "Book issued", warna → "Cannot issue book"
+
+// p1
+let hasLibraryCard = true;
+let booksAlreadyIssued = true;
+
+if (hasLibraryCard && booksAlreadyIssued){
+    console.log("Book issued");
+}
+else{
+    console.log("Cannot issue book");
+}
+
+
+
+// && — Problem 2
+// Ek gym membership checker: entry milegi tabhi jab:
+// hasMembership (boolean) true ho, aur
+// feePaid (boolean) true ho
+// Dono true → "Welcome to the gym", warna → "Access denied"
+// p2
+
+let hasMembership = true

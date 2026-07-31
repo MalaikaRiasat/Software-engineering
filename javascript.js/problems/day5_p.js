1b2e86deedd4a6743647a3e7620e0fcd4fbde742
Malaika Riasat <malaika.202404824@gcuf.edu.pk>
2026-07-31 23:40:13 +0500

    COMMIT

---
+++ javascript.js/problems/day5_p.js
@@ -0,0 +1,94 @@
+// let n=1;
+
+// while(n<=5){
+//     console.log(n);
+//     n++;
+// }
+
+// let i= 5;
+
+// while(i>=1){
+//     console.log(i);
+//     i--;
+// }
+
+// let e=2;
+ 
+// while(e<=10){
+//     console.log(e);
+//     e=e+2;
+// }
+
+// let o=1;
+
+// while(o<=10){
+//     console.log(o);
+//     o=o+2;
+// }
+
+// let x=3;
+// let y=1;
+
+// while (y<=10) {
+//     console.log(x*y);
+//     y++;
+// }
+
+
+
+// let u=100;
+
+// while(u>=0){
+//     console.log(u);
+//     u=u-10
+// }
+
+//functions
+
+function sayhellow(){
+    console.log("Hallow");
+}
+
+sayhellow();
+
+function study(){
+    console.log("i am studing javascript daily");
+}
+study();
+
+
+
+// jab mujy koi part reuse karna pary ga to ma usy ak function bana ka reuse karun gi multi time
+
+
+
+function student(name){
+    console.log("Hellow:" + name);
+
+}
+
+student("Malaika");
+student("Aima");
+student("Amina");
+student("Emaan");
+
+
+// H.W
+// Q1:
+function wellcom(name){
+    console.log("Wellcome:"+name );
+}
+
+wellcom("Malaika");
+wellcom("Ahmad");
+
+// Q2:
+
+function square(num) {
+    console.log(num*num);
+    
+}
+
+square(5);
+square(10);
+square(12);

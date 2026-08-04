8bfd74cc295daad3251668fdf15e31899d9d98e5
Malaika Riasat <malaika.202404824@gcuf.edu.pk>
2026-08-04 18:48:59 +0500

    c

---
+++ javascript.js/problems/day7_p.js
@@ -0,0 +1,42 @@
+// let colors = ["Red","Green","Blue","Black"];
+
+// for(let i=0; i < colors.length;i++){
+//     console.log(colors[i]);
+// }
+
+
+
+// let students = ["Ali","Sara","Malaika","Ahmad"];
+
+// for(let z=0; z < students.length; z++ ){
+//     console.log(students[z]);
+// }
+
+// let numbers =[10,20,30,40,50];
+
+// for(let n=0; n < numbers.length;n++){
+//     console.log(numbers[n]);
+// }
+
+
+// let marks = ["70","80","90","100"];
+// for (let m=0; m < marks.length;m++){
+
+
+//     console.log("Student",m+1,":",marks[m]);
+    
+// }
+
+
+
+let fruits=["Apple","Banana","Orange"];
+
+for(let fruit of fruits){
+    console.log(fruit);
+}
+
+let colors = ["Red", "Green", "Blue"];
+for(let color of colors);{
+    console.log
+
+}

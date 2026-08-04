8c3ad9d90cef0229a805b6f0f6fb03728469744a
Malaika Riasat <malaika.202404824@gcuf.edu.pk>
2026-08-05 01:31:22 +0500

    c

---
+++ javascript.js/problems/day9_p.js
@@ -0,0 +1,13 @@
+//Arry  slice()
+//p1
+let cities=["Lahore","Karachi","Islamabad","Multan"];
+
+let Ncities = cities.slice(1,3);
+console.log(Ncities);
+
+
+let marks = [10,20,30,40,50];
+
+let Nmarks = marks.slice(1,4);
+
+console.log(Nmarks);

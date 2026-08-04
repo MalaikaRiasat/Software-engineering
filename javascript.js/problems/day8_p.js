6026ff144dfc74de5ccfa4dcaaf0d7c6d840b86b
Malaika Riasat <malaika.202404824@gcuf.edu.pk>
2026-08-04 23:58:35 +0500

    c

--- javascript.js/problems/day8_p.js
+++ javascript.js/problems/day8_p.js
@@ -1,1 +1,34 @@
 // objects
+let Book ={
+    title:"peer-e-kamel",
+    author:"Nimra Ahmad",
+    pages:1750,
+    price:1500
+}
+console.log("Title:",Book.title);
+console.log("Price:",Book.price);
+console.log("Page:",Book.page);
+console.log("author:",Book.author);
+
+
+let Laptop ={
+    brand:"hp",
+    model:"5th",
+    ram:"4GB",
+    price:70000
+}
+console.log("brand:",Laptop.brand);
+console.log("modal:",Laptop.model);
+console.log("ram:",Laptop.ram);
+console.log("price:",Laptop.price);
+
+
+let teacher={
+    name:"Ali",
+    Subject:"javaScript",
+    experience: 5,
+}
+
+console.log(teacher.name);
+console.log(teacher.Subject);
+console.log(teacher.Subjec);

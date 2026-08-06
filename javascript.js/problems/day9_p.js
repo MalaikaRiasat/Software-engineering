d9aea56655092e6f00a0028da62d70bfdc3242a4
Malaika Riasat <malaika.202404824@gcuf.edu.pk>
2026-08-06 23:18:34 +0500

    g

--- javascript.js/problems/day9_p.js
+++ javascript.js/problems/day9_p.js
@@ -5,9 +5,25 @@ let cities=["Lahore","Karachi","Islamabad","Multan"];
 let Ncities = cities.slice(1,3);
 console.log(Ncities);
 
-
+//p2
 let marks = [10,20,30,40,50];
 
 let Nmarks = marks.slice(1,4);
 
 console.log(Nmarks);
+
+//p3
+let skills = ["HTML","CSS","Javascript","React"];
+let Nskills = skills.slice(0,2);
+
+console.log(Nskills);
+
+
+
+//Challenge
+
+let months = ["Jan","Feb","Mar","Apr","May","Jun"];
+
+let Nmonths= months.slice(2,5);
+console.log(Nmonths);
+

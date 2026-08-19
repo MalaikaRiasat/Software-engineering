cff98b6dc47b592e051a41f520503e0c83c71e10
Malaika Riasat <malaika.202404824@gcuf.edu.pk>
2026-08-19 12:16:54 +0500

    c

--- javascript.js/problems/day9_p.js
+++ javascript.js/problems/day9_p.js
@@ -27,19 +27,51 @@
 // let Nmonths= months.slice(2,5);
 // console.log(Nmonths);
 
-//splice
+// //splice
+// //delete
+// // p1
+// let colours =["Red","Green","Blue","Black"];
+// colours.splice(1,1);
+// console.log(colours);
 
-// p1
-let colours =["Red","Green","Blue","Black"];
-colours.splice(1,1);
-console.log(colours);
+// //p2
+// let numbers =[10,20,30,40,50];
 
-//p2
-let numbers =[10,20,30,40,50];
+// numbers.splice(2,2);
+// console.log(numbers);
 
-numbers.splice(2,2);
+// //add
+// //p1
+// let colors = ["Red","Blue","Green"];
+// colors.splice(1,0,"Yellow");
+// console.log(colors);
+
+// //p2
+
+// let numbers = [10,20,40,50]
+// numbers.splice(2,0,30);
+// console.log(numbers);
+
+// //p3
+// let skills =["HTML","JavaScript"];
+// skills.splice(1,0,"CSS");
+// console.log(skills);
+
+//Replace
+//p1
+
+let colors = ["Red","Green","Blue"];
+colors.splice(1,1,"Yellow");
+console.log(colors);
+
+
+// p2
+let numbers =[10,20,30,40];
+numbers.splice(2,1,35);
 console.log(numbers);
 
 
-
-
+// p3
+let skills = ["HTML","CSS","Javascript"];
+skills.splice(1,1,"Bootstrap");
+console.log(skills);

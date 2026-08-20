122efb281c672ed6179881c82ad56c9f10722455
Malaika Riasat <malaika.202404824@gcuf.edu.pk>
2026-08-21 00:25:58 +0500

    c

--- javascript.js/problems/day9_p.js
+++ javascript.js/problems/day9_p.js
@@ -57,21 +57,94 @@
 // skills.splice(1,0,"CSS");
 // console.log(skills);
 
-//Replace
-//p1
+// //Replace
+// //p1
 
-let colors = ["Red","Green","Blue"];
-colors.splice(1,1,"Yellow");
-console.log(colors);
+// let colors = ["Red","Green","Blue"];
+// colors.splice(1,1,"Yellow");
+// console.log(colors);
 
 
-// p2
-let numbers =[10,20,30,40];
-numbers.splice(2,1,35);
+// // p2
+// let numbers =[10,20,30,40];
+// numbers.splice(2,1,35);
+// console.log(numbers);
+
+
+// // p3
+// let skills = ["HTML","CSS","Javascript"];
+// skills.splice(1,1,"Bootstrap");
+// console.log(skills);
+
+
+
+// // includes
+// // p1
+// let colors = ["Red","Green","Blue"];
+// console.log(colors.includes("Green"));
+
+// // p2
+// let students =["Ali","Sara","Malaika","Ahmad"];
+// console.log(students.includes("Ayesha"));
+
+// // p3
+// let skills= ["HTML","CSS","JavaScript"];
+// console.log(skills.includes("React"));
+// console.log(skills.includes("CSS"));
+
+
+
+// //indexof
+// // p1
+// let fruits =["Apple", "Banana","Mango","Orange"];
+// console.log(fruits.indexOf("Mango"));
+
+
+// // p2
+// let cities =["Lahore","Karachi","Islamabad","Multan"];
+// console.log(cities.indexOf("islamabad"));
+
+// // p3
+// let skills =["HTML","CSS","JavaScript","React"];
+// console.log(skills.indexOf("React"));
+
+// let colors =["Red","Green","Blue"];
+// console.log(colors.indexOf("Yellow"));
+
+
+
+
+
+// 
+
+// finals
+
+
+// 🔥 Challenge 1
+// let fruits = ["Apple", "Banana", "Mango", "Orange"];
+// Banana ko Grapes se replace karo.
+// Apple check karo ke array mein hai ya nahi.
+// Mango ka index find karo.
+
+let fruits = ["Apple", "Banana", "Mango", "Orange"];
+fruits.splice(1,1,"Grapes");
+console.log(fruits);
+console.log(fruits.includes("Apple"));
+console.log(fruits.indexOf("Mango"));
+
+
+
+🔥 Challenge 2
+let numbers = [10, 20, 30, 40, 50];
+30 aur 40 delete karo.
+25 ko 10 aur 20 ke beech add karo.
+50 ka index find karo.
+
+let numbers = [10, 20, 30, 40, 50];
+numbers.splice(2,2);
+numbers.splice(1,0,25);
 console.log(numbers);
+console.log(numbers.indexOf(50));
 
 
-// p3
-let skills = ["HTML","CSS","Javascript"];
-skills.splice(1,1,"Bootstrap");
-console.log(skills);
+
